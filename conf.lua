0234ff6a0d5e9196cecf2fa8f0a81e1d0672630e
José Augusto <augustoreal.santos@gmail.com>
2026-03-12 12:23:14 -0300

    conf.lua update

--- conf.lua
+++ conf.lua
@@ -1,21 +1,17 @@
 function love.conf(t)
-    t.identity = nil                    -- The name of the save directory (string)
     t.version = "11.5"                  -- The LÖVE version this game was made for (string)
     t.console = false                   -- Attach a console (boolean, Windows only)
-    t.accelerometerjoystick = false      -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
+    t.accelerometerjoystick = false     -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
 
 
-    t.audio.mixwithsystem = false        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
+    t.audio.mixwithsystem = false       -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
 
-    t.window.title = "Shooting Gallery"         -- The window title (string)
+    t.window.title = "Shooting Gallery" -- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
     t.window.width = 800                -- The window width (number)
     t.window.height = 600               -- The window height (number)
-    t.window.resizable = false          -- Let the window be user-resizable (boolean)
     t.window.minwidth = 1               -- Minimum window width if the window is resizable (number)
     t.window.minheight = 1              -- Minimum window height if the window is resizable (number)
-    t.window.fullscreen = false         -- Enable fullscreen (boolean)
-    t.window.fullscreentype = "desktop" -- Choose between "desktop" fullscreen or "exclusive" fullscreen mode (string)
     t.window.x = nil                    -- The x-coordinate of the window's position in the specified display (number)
     t.window.y = nil                    -- The y-coordinate of the window's position in the specified display (number)
 
@@ -36,5 +32,4 @@ function love.conf(t)
     t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
     t.modules.touch = false              -- Enable the touch module (boolean)
     t.modules.video = false              -- Enable the video module (boolean)
-    t.modules.window = true  
 end
